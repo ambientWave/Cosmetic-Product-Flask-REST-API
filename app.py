31a74b6515d5ab855d8bac10384d1b307fe31add
Mena Samer <140031905+ambientWave@users.noreply.github.com>
2023-08-08 17:23:45 +0400

    Update app.py

--- app.py
+++ app.py
@@ -52,46 +52,6 @@ def add_cosmetic():
     return {"result": "success - item is added", "added_item": {"Brand": cosmetic.brand, "Product Line Name": cosmetic.product_line_name,
                          "Product Line Category": cosmetic.product_line_category}}
 
-'''POST requests can be done using:
-1- CURL on Unix
-
-CURL is awesome to do what you want! It's a simple, but effective, command line tool.
-
-REST implementation test commands:
-
-curl -i -X GET http://rest-api.io/items
-curl -i -X GET http://rest-api.io/items/5069b47aa892630aae059584
-curl -i -X DELETE http://rest-api.io/items/5069b47aa892630aae059584
-curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "New item", "year": "2009"}' http://rest-api.io/items
-curl -i -X PUT -H 'Content-Type: application/json' -d '{"name": "Updated item", "year": "2010"}' http://rest-api.io/items/5069b47aa892630aae059584
-
-2- javascript
-async function postData(url = "", data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-		mode: "cors",
-    headers: {
-            "Content-Security-Policy": "script-src 'self'; connect-src 'self';",
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-postData("http://127.0.0.1:5000/cosmetics/add_item", {
-"brand": "Oz Naturals",
-"product_line_name": "Serum",
-"product_line_category": "skin/moisturizers"
-}).then((data) => {
-  console.log(data); // JSON data parsed by `data.json()` call
-});
-'''
 
 @app.route('/cosmetics/<id>/', methods=['DELETE'])
 def delete_cosmetic_by_id(id):
